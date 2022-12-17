09be9898013f1c5170c052b7e907930488122e33
Neige <72865616+Neige7@users.noreply.github.com>
2022-12-17 19:56:43 +0800

    add pdf converter

---
+++ docs/.docsifytopdfrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    contents: [ "_sidebar.md" ],
+    pathToPublic: "pdf/readme.pdf",
+    pdfOptions: "<options for puppeteer.pdf()>",
+    removeTemp: true,
+    emulateMedia: "screen",
+  }
---
+++ docs/how to use pdf converter.md
@@ -0,0 +1,5 @@
+第一步：安装nodejs
+
+第二步：npm install --save-dev docsify-pdf-converter
+
+第三步：node main.js
